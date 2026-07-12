f382def49924899e76a37827538c2850ca073f05
leelu74 <leeladhar679@gmail.com>
2026-07-12 14:21:25 +0530

    Revert "fix(employee): restrict employee creation to admin only"
    This reverts commit 5950def64a89185a2c675fe872552d80a2f3d550.

--- frontend/src/pages/employees/EmployeeList.tsx
+++ frontend/src/pages/employees/EmployeeList.tsx
@@ -144,12 +144,9 @@ export default function EmployeeList() {
                 onChange={handleImportFile} />
             </>
           )}
-          {/* Only Admin/Master can add employees - Supervisors can only view/monitor */}
-          {canManage && (
-            <button className="btn btn-primary" onClick={() => navigate("/employees/new")}>
-              + Add Employee
-            </button>
-          )}
+          <button className="btn btn-primary" onClick={() => navigate("/employees/new")}>
+            + Add Employee
+          </button>
         </div>
       </div>
 
